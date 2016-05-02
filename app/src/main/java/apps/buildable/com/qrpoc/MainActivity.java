f6f3c93eda62220c013b26af471976634909ee75
Sam.Z.D <samerzmd@gmail.com>
2016-05-02 20:05:01 +0300

    complete

--- app/src/main/java/apps/buildable/com/qrpoc/MainActivity.java
+++ app/src/main/java/apps/buildable/com/qrpoc/MainActivity.java
@@ -1,13 +1,54 @@
 package apps.buildable.com.qrpoc;
 
+import android.graphics.PointF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+
+public class MainActivity extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener {
+
+    private TextView myTextView;
+    private QRCodeReaderView mydecoderview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mydecoderview = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
+        mydecoderview.setOnQRCodeReadListener(this);
+
+        myTextView = (TextView) findViewById(R.id.exampleTextView);
+    }
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        myTextView.setText(text);
+    }
+
+
+    // Called when your device have no camera
+    @Override
+    public void cameraNotFound() {
+
+    }
+
+    // Called when there's no QR codes in the camera preview image
+    @Override
+    public void QRCodeNotFoundOnCamImage() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mydecoderview.getCameraManager().startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mydecoderview.getCameraManager().stopPreview();
     }
 }
